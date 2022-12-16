2e93de1a6e8b085bbf06bb33a692be95d82624fc
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-16 17:14:06 +0800

    adding register function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Nav from "../components/Nav";
+import Nav from "./components/Nav";
 import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
-import Footer from "../components/Footer";
-
+import Footer from "./components/Footer";
+import Signup from "./components/SignUp";
 function App() {
   return (
     <div className="App">
@@ -15,6 +15,7 @@ function App() {
           <Route path="/update" element={<h1>Update Product Components</h1>} />
           <Route path="/logout" element={<h1>Logout Components</h1>} />
           <Route path="/profile" element={<h1>Profile Components</h1>} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
       <Footer />
