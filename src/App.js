4c63f0ff2910914bc6d0fa69c85524b4e969e5c1
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-16 16:51:38 +0800

    adding footer and header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./Nav";
+import Nav from "../components/Nav";
 import { BrowserRouter, Routes, Router, Route } from "react-router-dom";
+import Footer from "../components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/profile" element={<h1>Profile Components</h1>} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
