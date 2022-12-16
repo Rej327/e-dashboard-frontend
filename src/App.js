ad004458c5e3a057924e3f4809137f0473f6858d
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-16 16:13:04 +0800

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>E-DASHBOARD</h1>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>Header file</div>;
+};
+
+export default Nav;
